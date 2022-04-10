1d85172a7e8ce19d8d3d7d3bc0cfeb1134e38d87
osha7 <ogroetz@gmail.com>
2022-04-10 23:26:00 +0000

    tests pass

--- hash_find_value.rb
+++ hash_find_value.rb
@@ -7,3 +7,10 @@
 sample_hash = {:a => 100, :b => 200, :c => 300, :d => 400, :e => rand(200), :f => 600, :g => 0 }
 
 p "Enter an integer to find:"
+user_input = gets.chomp.to_i
+location = sample_hash.key(user_input)
+if location
+  p "#{user_input} is under the key: #{location}."
+else
+  p "Could not find the integer #{user_input}"
+end
--- hash_person.rb
+++ hash_person.rb
@@ -1,2 +1,9 @@
 # Ask for a name, age, and occupation; store the values in a Hash; display the Hash.
 p "Enter a name, age, and occupation separated by spaces:"
+user_input_array = gets.chomp.split(" ")
+person = Hash.new
+person[:name] = user_input_array[0]
+person[:age] = user_input_array[1].to_i
+person[:occupation] = user_input_array[2]
+
+p person
